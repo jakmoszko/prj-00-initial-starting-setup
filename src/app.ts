c276946cf900eefdea5033406a9c17282b4d4abb
Kuba <jak.moszkowicz@gmail.com>
2024-01-19 06:45:07 +0100

    Fetching User Input

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ class ProjectInput {
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
     titleInputElement: HTMLInputElement;
-    decriptionInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
     peopleInputElement: HTMLInputElement;
 
     constructor() {
@@ -29,17 +29,45 @@ class ProjectInput {
         this.element.id = 'user-input';
 
         this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
-        this.decriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
         this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
 
         this.configure();
         this.attach();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this. titleInputElement.value
+        const enteredDescription = this. descriptionInputElement.value
+        const enteredPeople = this. peopleInputElement.value
+
+        if (
+            enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0 ||
+            enteredPeople.trim().length === 0
+        ) {
+            alert('Please fill in all fields');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
     @autobind
     private submitHandler(event: Event){
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
     }
 
     private configure() {
